47120cf8a10d508c3b41d8096d32c8cd269627ca
nirmalkumarraveendranath <nirmal198939@gmail.com>
2019-06-15 11:02:17 -0400

    Corrections

--- narrative/public/view_conditions/httprequests.js
+++ narrative/public/view_conditions/httprequests.js
@@ -83,23 +83,3 @@ function ajaxRequestPredict(textValue, predictValue1, predictValue2, type, theme
     }
 }
 
-(function($) {
-    $.fn.inputFilter = function(inputFilter) {
-        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-            if (inputFilter(this.value)) {
-                this.oldValue = this.value;
-                this.oldSelectionStart = this.selectionStart;
-                this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
-                this.value = this.oldValue;
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-            }
-        });
-    };
-}(jQuery));
-
-setInputFilter(document.getElementById("pred2"), function(value) {
-    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 99); });
-
-setInputFilter(document.getElementById("pred1"), function(value) {
-    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 99); });
